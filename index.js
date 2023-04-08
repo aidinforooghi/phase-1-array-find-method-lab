18688115d0548176118642975cb42793c853875e
Aidin <aidin.forooghi@gmail.com>
2023-04-08 10:33:22 -0400

    Passed 2 Tests

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function superbowlWin(record) {
+    const winRecord = record.find(obj => obj.result === 'W');
+
+    if (winRecord) {
+        return winRecord.year;
+    } else {
+        return undefined;
+    }
+}
